c2248573fb00815250333359f62bb2e6185626b9
Tatiana Zhuravleva <zhuravlini@gmail.com>
2022-10-13 19:08:24 +0300

    исправленный код

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
 
-        int a = 15_678; //стоимость билета
-        int b = 20; // кол-во рублей за 1 милю
-        int miles = a / b; //расчет количества миль
+        int ticketPrice = 15_678; //стоимость билета
+        int divider = 20; // кол-во рублей за 1 милю
+        int miles = ticketPrice / divider; //расчет количества миль
 
         System.out.println(" Вам начислено " + miles + " бонусных миль");
 
